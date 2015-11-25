0e8f69573dc54247d2bb25f300d2def9d3b26f92
AndyWang <andywang1116@gmailcom>
2015-11-25 20:39:37 +0800

    建立訂單(顯示訂單內容,token產生)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,18 @@ class OrdersController < ApplicationController
     if @order.save
       @order.build_item_cache_from_cart(current_cart)   # @order呼叫 order.rb裡定義的function, 產生cache(日後產品可能會被刪掉)
       @order.calculate_total!(current_cart)             # 將目前購物車總金額的值存進訂單的金額欄位(total)
-      redirect_to order_path(@order)
+      redirect_to order_path(@order.token)
     else
       render "carts/checkout"   # 跨controller時(這邊是orders,要跨到carts), 且複用template , 在同一controller裡則是render :new
     end
+
+    def show
+      @order = Order.find_by_token(params[:id])
+      @order_info = @order.info
+      @order_items = @order.items
+    end
+
+
   end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,8 @@ class Order < ActiveRecord::Base
   # 當頁面同時送出form給兩個model時，也就是巢狀表單，則要在model內進行宣告。這裡是對model order宣告接受model orderinfo 的巢狀屬性
   accepts_nested_attributes_for :info
 
+  before_create :generate_token
+
   def build_item_cache_from_cart(cart)
     cart.items.each do |cart_item|       # 這邊的items指的是model order_item 所以會有product_name price quantity等欄位可呼叫
       item = items.build                 # 新創一個order_item的object 叫做item
@@ -20,6 +22,11 @@ class Order < ActiveRecord::Base
   def calculate_total!(cart)
     self.total = cart.total_price  # 呼叫cart內的total_price, 存進被呼叫的order本身的total欄位
     self.save  # 儲存進db
+  end
+
+  # 定義 generate_token這個method, 使用內建SecureRandoom.uuid 產生出特定的值 存進目前order本身的token欄位
+  def generate_token
+    self.token = SecureRandom.uuid
   end
 
 end
