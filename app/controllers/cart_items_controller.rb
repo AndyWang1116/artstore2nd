2f5b3d3ec58ca6dff87257358ddf99a89eb66127
AndyWang <andywang1116@gmailcom>
2015-11-26 15:55:51 +0800

    購物車加入'數量'設計(基礎設定)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,4 +9,20 @@ class CartItemsController < ApplicationController
     redirect_to :back
   end
 
+  def update
+    @cart = current_cart
+    @item = @cart.cart_items.find_by(product_id: params[:id])   # 對curren_cart搜尋product_id為目前網址輸入的數字的cart_item,傳給@item
+
+    @item.update(item_params)  # 遇到update需要使用strong_parameter概念,讓指定欄位通過
+
+    redirect_to carts_path
+  end
+
+  private
+
+    # 使用item_param這個fuction時，對cart_item db做更改只有quantity這欄位可通過
+    def item_params
+      params.require(:cart_item).permit(:quantity)
+    end
+
 end
